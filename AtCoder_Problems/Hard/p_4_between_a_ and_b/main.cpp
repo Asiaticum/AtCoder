3c52cb02cbddf0a48e24138b9d7bff15e3e4ca53
Yusuke Hamanaka <asiaticum@YusukenoMacBook-Air.local>
2021-08-17 22:48:53 +0900

    commit all

---
+++ AtCoder_Problems/Hard/p_4_between_a_ and_b/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const ll LINF = 1e18;
+template <class T>
+bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+ll round_up(ll a, ll x) { return (a + x - 1) / x; }
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll a, b, x;
+    cin >> a >> b >> x;
+
+    ll diff;
+    if (a % x == 0) {
+        diff = b - a + 1;
+    } else {
+        diff = b - a;
+    }
+
+    ll ans = round_up(diff, x);
+
+    cout << ans << endl;
+
+    return 0;
+}
